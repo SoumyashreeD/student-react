17d70adb229dc9105396626f7ca182f69c053ecd
SoumyashreeD <48245890+SoumyashreeD@users.noreply.github.com>
2020-03-12 15:34:34 +0530

    Update AddComponent.jsx

--- src/component/AddComponent.jsx
+++ src/component/AddComponent.jsx
@@ -11,6 +11,7 @@ class AddComponent extends Component{
             firstName: '',
             lastName: '',
             age: '',
+            gender:'',
             percentage: '',
             message: null
         }
@@ -19,7 +20,7 @@ class AddComponent extends Component{
 
     saveStudent = (e) => {
         e.preventDefault();
-        let student= {username: this.state.username, password: this.state.password, firstName: this.state.firstName, lastName: this.state.lastName, age: this.state.age, percentage: this.state.percentage};
+        let student= {username: this.state.username, password: this.state.password, firstName: this.state.firstName, lastName: this.state.lastName, age: this.state.age,gender: this.state.gender, percentage: this.state.percentage};
         AppService.addStudent(student)
             .then(res => {
                 this.setState({message : 'Student added successfully.'});
@@ -29,6 +30,11 @@ class AddComponent extends Component{
 
     onChange = (e) =>
         this.setState({ [e.target.name]: e.target.value });
+        onRadioChange = (e) => {
+            this.setState({
+              gender: e.target.value
+            });
+          }
 
     render() {
         return(
@@ -36,39 +42,59 @@ class AddComponent extends Component{
                 <h2 className="text-center">Add Student</h2>
                 <form>
                 <div className="form-group">
-                    <label>User Name:</label>
+                    <label>User Name:</label><br></br>
                     <input type="text" placeholder="username" name="username" className="form-control" value={this.state.username} onChange={this.onChange}/>
                 </div>
 
                 <div className="form-group">
-                    <label>Password:</label>
+                    <label>Password:</label><br></br>
                     <input type="password" placeholder="password" name="password" className="form-control" value={this.state.password} onChange={this.onChange}/>
                 </div>
 
                 <div className="form-group">
-                    <label>First Name:</label>
+                    <label>First Name:</label><br></br>
                     <input placeholder="First Name" name="firstName" className="form-control" value={this.state.firstName} onChange={this.onChange}/>
                 </div>
 
                 <div className="form-group">
-                    <label>Last Name:</label>
+                    <label>Last Name:</label><br></br>
                     <input placeholder="Last name" name="lastName" className="form-control" value={this.state.lastName} onChange={this.onChange}/>
                 </div>
 
                 <div className="form-group">
-                    <label>Age:</label>
+                    <label>Age:</label><br></br>
                     <input type="number" placeholder="age" name="age" className="form-control" value={this.state.age} onChange={this.onChange}/>
-                
-                
-                </div>
-                
+                 </div>
+                 <div check>
+          <input
+            type="radio"
+            value="male" // this is te value which will be picked up after radio button change
+            checked={this.state.gender === "male"} // when this is true it show the male radio button in checked 
+            onChange={this.onRadioChange} // whenever it changes from checked to uncheck or via-versa it goes to the handleRadioChange function
+          />
+          <span
+           style={{ marginLeft: "5px" }} // inline style in reactjs 
+          >Male</span>
+        </div>
+        <div check>
+          <input
+            type="radio"
+            value="female"
+            checked={this.state.gender === "female"}
+            onChange={this.onRadioChange}
+          />
+          <span style={{ marginLeft: "5px" }}>Female</span>
+          <br></br>
+        </div>
+         
 
                 <div className="form-group">
-                    <label>Percentage:</label>
+                    <label>Percentage:</label><br></br>
                     <input type="number" placeholder="percentage" name="percentage" className="form-control" value={this.state.percentage} onChange={this.onChange}/>
                 </div>
 
-                <button className="btn btn-success" onClick={this.saveStudent}>Save</button>
+                <button className="btn btn-success" onClick={this.saveStudent} >Save</button>
+            
             </form>
     </div>
         );
